f6d4c721b901f8c35930d23859bb6e6b5dfea2f6
+ <brandi.ude@gmail.com>
2021-11-11 21:27:51 -0500

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,48 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasksStateArray, setTasksStateArray] =useState(TASKS);
+  const [selectedCategory, setSelectedCategory]=useState("All");
+
+  function handleTaskDelete(text){
+    //remove task via filter
+    const newTaskArray = tasksStateArray.filter((task) => task.text !== text);
+    setTasksStateArray(newTaskArray);
+  }
+
+  function handleTaskAdd(newTask){
+    const newTaskArray = [...tasksStateArray, newTask]
+    setTasksStateArray(newTaskArray);
+  }
+
+  function handleCategorySelect(newSelectedCategory) {
+    setSelectedCategory(newSelectedCategory)
+    
+    console.log("button clicked")
+  }
+
+  const filteredTasks = tasksStateArray.filter((task) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return task.category === selectedCategory;
+    }
+  }); 
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+      categories={CATEGORIES}
+      selectedCategory={selectedCategory}
+      handleCategorySelect={handleCategorySelect}/>
+      <NewTaskForm 
+      categories={CATEGORIES.filter(cat => cat !== "All")}
+      handleTaskAdd={handleTaskAdd}
+      />
+      <TaskList 
+      tasks={filteredTasks} 
+      handleTaskDelete={handleTaskDelete}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, handleCategorySelect, selectedCategory}) {
+  const buttonElements = categories.map(category=>{
+
+    return(
+       <button 
+        className={category === selectedCategory ? "selected" : null}
+        key={category} 
+        onClick={()=> handleCategorySelect(category)} >
+          {category}
+        </button>)
+  });
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
+      {buttonElements}
       {/* render <button> elements for each category here */}
     </div>
   );
